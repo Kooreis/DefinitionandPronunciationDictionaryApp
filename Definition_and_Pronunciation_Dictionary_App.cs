be2a8696d682be70f0d41771e0448ce76c1f9eb0
Kooreis <kooreis.official@gmail.com>
2022-03-15 02:00:00 -0500

    Adds two entries to the dictionary: 'apple' and 'banana', each with a definition and pronunciation.

--- Definition_and_Pronunciation_Dictionary_App.cs
+++ Definition_and_Pronunciation_Dictionary_App.cs
@@ -1,10 +1,2 @@
-using System;
-using System.Collections.Generic;
-
-class Program
-{
-    static void Main(string[] args)
-    {
-        Dictionary<string, Tuple<string, string>> dictionary = new Dictionary<string, Tuple<string, string>>();
-    }
-}
+dictionary.Add("apple", new Tuple<string, string>("A round fruit with red or yellow or green skin", "/ˈæpəl/"));
+dictionary.Add("banana", new Tuple<string, string>("A long curved fruit with yellow skin", "/bəˈnænə/"));
