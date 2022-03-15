5046fa99e9b7b9f75e1c2545e0c73a4f6ba3ca0b
Kooreis <kooreis.official@gmail.com>
2022-03-15 02:00:00 -0500

    In this update, additional libraries are imported and a function to get the pronunciation of a word from an API is implemented. The pronunciation is saved as an mp3 file and played.

--- Definition_and_Pronunciation_Dictionary_App.py
+++ Definition_and_Pronunciation_Dictionary_App.py
@@ -1,8 +1,11 @@
-import json
-import requests
+from gtts import gTTS
+import os
 
-
-def get_definition(word):
+def get_pronunciation(word):
     response = requests.get(f"https://api.dictionaryapi.dev/api/v2/entries/en/{word}")
     data = json.loads(response.text)
-    return data[0]['meanings'][0]['definitions'][0]['definition']
+    pronunciation = data[0]['phonetics'][0]['text']
+    audio_link = data[0]['phonetics'][0]['audio']
+    tts = gTTS(text=pronunciation, lang='en')
+    tts.save("pronunciation.mp3")
+    os.system("mpg321 pronunciation.mp3")
