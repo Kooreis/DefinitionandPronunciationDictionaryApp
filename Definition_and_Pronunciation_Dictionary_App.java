79016b2704db874e62d32a69883b2ee0d8ee7af9
Kooreis <kooreis.official@gmail.com>
2022-03-15 02:00:00 -0500

    Defines the main class and method, and initializes the dictionary as a HashMap.

--- Definition_and_Pronunciation_Dictionary_App.java
+++ Definition_and_Pronunciation_Dictionary_App.java
@@ -1,3 +1,3 @@
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
+public class DictionaryApp {
+    public static void main(String[] args) {
+        Map<String, String[]> dictionary = new HashMap<>();
